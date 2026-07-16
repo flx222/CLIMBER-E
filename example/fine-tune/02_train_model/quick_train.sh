18cdc0f9204e82cb5638c42ca7e2a3b200119edc
flx222 <120375950+flx222@users.noreply.github.com>
2026-07-16 17:37:02 +0800

    quick_train.sh
    Signed-off-by: flx222 <120375950+flx222@users.noreply.github.com>

--- example/fine-tune/02_train_model/quick_train.sh
+++ example/fine-tune/02_train_model/quick_train.sh
@@ -5,10 +5,10 @@ run_task () {
     local output_url="${data_url}"
 
     # Train
-    python mpbert_regress_smile.py \
+    python run_regress.py \
         --config_path config_2048_sm.yaml \
         --data_url "${data_url}" \
-        --load_checkpoint_url "${checkpoint_url}" \
+        --load_checkpoint_url run_regress"${checkpoint_url}" \
         --output_url "${output_url}" \
         --device_id 5 \
         --do_train True \
@@ -16,7 +16,7 @@ run_task () {
         1> "${data_url}/train.log" 2> "${data_url}/train_sys.log"
 
     # Evaluate
-    python mpbert_regress_smile.py \
+    python run_regress.py \
         --config_path config_2048_sm.yaml \
         --data_url "${data_url}" \
         --load_checkpoint_url "${data_url}" \
